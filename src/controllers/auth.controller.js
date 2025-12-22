88ffb66e66bbe91987b2b412ad0f98029722b163
davmik_skillex <david.mikayelyan@skillex.am>
2025-12-22 12:58:39 +0400

    ignore this (comments removal)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,8 +6,6 @@ class AuthController {
    * @param {import('express').Request & {body: RegisterBodySchema}} req
    */
   async register(req) {
-    // here we can check some manual validations
-
     return authService.register(req.body)
   }
 
@@ -16,8 +14,6 @@ class AuthController {
    * @param {import('express').Request & {body: LoginBodySchema}} req
    */
   async login(req) {
-    // here we can check some manual validations
-
     return authService.login(req.body)
   }
 }
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,6 @@ class UserController {
    * @param {AuthRequestType} req
    */
   async getMe(req) {
-    // if needed, we can get and fill other data from user here
     return {
       ...req.user,
       // other custom fields here if needed
