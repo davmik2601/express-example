b01d3ffec903ddf822548a69102e3bbafaab3783
davmik_skillex <david.mikayelyan@skillex.am>
2026-05-07 19:10:43 +0400

    zod global types correction

--- types/zod.d.ts
+++ types/zod.d.ts
@@ -1,24 +1,15 @@
 import type {ZodTypeAny} from "zod"
 
 declare global {
-  type RequiredKeys<T> = {
-    [K in keyof T]-?: undefined extends T[K] ? never : K
-  }[keyof T]
-
-  type OptionalKeys<T> = {
-    [K in keyof T]-?: undefined extends T[K] ? K : never
-  }[keyof T]
-
   type IsAny<T> = 0 extends (1 & T) ? true : false
 
-  type IsFiniteStringKeyRecord<T> =
-    T extends object
-      ? keyof T extends string
-        ? string extends keyof T
-          ? false
-          : true
-        : false
-      : false
+  type RequiredKeys<T extends object> = keyof {
+    [K in keyof T as {} extends Pick<T, K> ? never : K]: T[K]
+  }
+
+  type OptionalKeys<T extends object> = keyof {
+    [K in keyof T as {} extends Pick<T, K> ? K : never]: T[K]
+  }
 
   // shallow version (top-level object shape only)
   type ZodSchemaFor<T> =
@@ -27,32 +18,33 @@ declare global {
       : T extends Date
         ? import("zod").ZodDate
         : T extends readonly (infer U)[]
-          ? import("zod").ZodArray<ZodSchemaFor<U>>
+          ? U extends object
+            ? import("zod").ZodArray<import("zod").ZodObject<ZodShapeFor<U>>>
+            : import("zod").ZodArray<ZodTypeAny>
           : T extends (infer U)[]
-            ? import("zod").ZodArray<ZodSchemaFor<U>>
+            ? U extends object
+              ? import("zod").ZodArray<import("zod").ZodObject<ZodShapeFor<U>>>
+              : import("zod").ZodArray<ZodTypeAny>
             : T extends object
               ? string extends keyof T
-                ? import("zod").ZodRecord<import("zod").ZodString, ZodSchemaFor<T[string]>>
+                ? ZodTypeAny
                 : number extends keyof T
-                  ? import("zod").ZodRecord<import("zod").ZodNumber, ZodSchemaFor<T[number]>>
+                  ? ZodTypeAny
                   : symbol extends keyof T
-                    ? import("zod").ZodRecord<import("zod").ZodSymbol, ZodSchemaFor<T[symbol]>>
-                    : IsFiniteStringKeyRecord<T> extends true
-                      ? (
-                        | import("zod").ZodObject<ZodShapeFor<T>>
-                        | import("zod").ZodRecord<
-                        import("zod").ZodEnum<Record<keyof T & string, keyof T & string>>,
-                        ZodSchemaFor<T[keyof T & string]>
-                      >
-                        )
-                      : import("zod").ZodObject<ZodShapeFor<T>>
+                    ? ZodTypeAny
+                    : import("zod").ZodObject<ZodShapeFor<T>>
               : ZodTypeAny
 
   type OptionalSchemaFor<T> =
     | ZodSchemaFor<T>
     | import("zod").ZodOptional<ZodSchemaFor<T>>
+    | import("zod").ZodNullable<ZodSchemaFor<T>>
+    | import("zod").ZodOptional<import("zod").ZodNullable<ZodSchemaFor<T>>>
+    | import("zod").ZodNullable<import("zod").ZodOptional<ZodSchemaFor<T>>>
+    | import("zod").ZodDefault<ZodSchemaFor<T>>
+    | import("zod").ZodDefault<import("zod").ZodOptional<ZodSchemaFor<T>>>
 
-  type ZodShapeFor<T> =
+  type ZodShapeFor<T extends object> =
     & { [K in RequiredKeys<T>]: ZodSchemaFor<Exclude<T[K], undefined>> }
     & { [K in OptionalKeys<T>]?: OptionalSchemaFor<Exclude<T[K], undefined>> }
 }
